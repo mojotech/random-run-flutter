08bec78fb8295e08948ed3e5a1564b7b1c8285eb
Jen Kaplan <jenniferckaplan@gmail.com>
2019-10-08 14:04:11 -0400

    update style for FirstScreen

--- lib/screens/FirstScreen.dart
+++ lib/screens/FirstScreen.dart
@@ -1,30 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:random_run/theme.dart' as T;
 
 import 'package:random_run/widgets/Picker.dart';
 import 'package:random_run/widgets/Dropdown.dart';
+import 'package:random_run/widgets/NextButton.dart';
 
 class FirstScreen extends StatelessWidget {
+  static Color brightPink = T.RandomRunColors.brightPink;
+
+  final TextStyle _howFarStyle = TextStyle(
+    fontSize: T.FontSize.small,
+    fontWeight: FontWeight.w500,
+    color: brightPink,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text('Random Run'),
+        title: T.appBar,
+        backgroundColor: brightPink,
       ),
-      body: new Flex(
-        direction: Axis.vertical,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Text('How far would you like to run?'),
-          Picker(),
-          Dropdown(),
-          new RaisedButton(
-            child: Text('Next'),
-            onPressed: () {
-              // Navigate to the second screen using a named route.
-              Navigator.pushNamed(context, '/second');
-            },
-          ),
-        ],
+      body: Center(
+        child: Flex(
+          direction: Axis.vertical,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                top: T.Spacing.medium,
+                bottom: T.Spacing.medium,
+              ),
+              child: Text(
+                'How far would you like to run?',
+                style: _howFarStyle,
+              ),
+            ),
+            Picker(),
+            Dropdown(),
+            Container(
+              padding: EdgeInsets.only(
+                top: T.Spacing.large,
+              ),
+              child: NextButton(),
+            ),
+          ],
+        ),
       ),
     );
   }
